d1e66ceb3c38858cc7954b69c44a277c91e3bdb8
RayyanUrRehman <iamrayyanurrehman2004@gmail.com>
2024-10-11 18:17:14 +0500

    update

---
+++ JavaScript/smit-test/app.js
@@ -0,0 +1,43 @@
+// q1
+// var name = prompt("Enter name")
+// var age = prompt("Enter age")
+
+// var str = "Hello, "+name+"! You are "+age+" years old."
+// alert(str)
+
+
+// q2
+// var num = +prompt("Enter number");
+// if (num > 0){
+//     alert("The number is positive")
+// }
+// else if(num < 0){
+//     alert("The number is negative")
+// }
+// else if(num==0){
+//     alert("The number is zero")
+// }
+
+
+// q3
+// var num = []
+// for (var i=0;i<5;i++){
+//     var number = +prompt("Enter number "+(i+1));
+//     num.push(number);
+// }
+
+// var sum=0;
+// for (var i=0;i<5;i++){
+//     sum += num[i];
+// }
+
+// var avg = sum/5;
+// alert("The average score is "+avg+".");
+
+
+// q4
+// var city = ["Karachi","Lahore","Islamabad","Quetta","Peshawar"];
+
+// for (var i=0;i<5;i++){
+//     console.log("City "+(i+1)+": "+city[i]);
+// }
